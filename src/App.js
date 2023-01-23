dac91339559fe643a67719e06473f55f1f5d6f3a
Peeyusha Shelake <peeyushelake123@gmail.com>
2023-01-23 16:14:04 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+  <h1>Ja<span class="highlight">mmm</span>ing</h1>
+  <div class="App">
+   
+    <div class="App-playlist">
     </div>
+  </div>
+</div>
   );
 }
 
